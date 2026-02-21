63688ea8ba6c63248115d7a3079b5348d9cebd09
Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>
2026-02-21 17:04:58 +0200

    chore: cleanup application
    Signed-off-by: Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import SearchBox from '@/components/SearchBox.vue';
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <SearchBox class="mx-auto mt-4"></SearchBox>
   </main>
 </template>
