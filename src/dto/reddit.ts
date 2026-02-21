bd608463cdaa67a73fda83a594e9c2bf65a9e854
Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>
2026-02-21 17:57:56 +0200

    feat: add media type declarations
    Signed-off-by: Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>

--- src/dto/reddit.ts
+++ src/dto/reddit.ts
@@ -24,6 +24,54 @@ export interface RedditPost {
   created_utc: number
   thumbnail: string
   permalink: string
+  domain: string
+  is_video: boolean
+  is_gallery: boolean
+  media?: {
+    reddit_video?: {
+      bitrates_mp4: number[]
+      fallback_url: string
+      height: number
+      width: number
+      duration: number
+    }
+  }
+  preview?: {
+    images: {
+      source: {
+        url: string
+        width: number
+        height: number
+      }
+      resolutions: {
+        url: string
+        width: number
+        height: number
+      }[]
+    }[]
+  }
+  gallery_data?: {
+    items: {
+      media_id: string
+      id: number
+    }[]
+  }
+  media_metadata?: Record<
+    string,
+    {
+      type: string
+      id: string
+      s?: {
+        x: number
+        y: number
+      }
+    }
+  >
+  secure_media_embed?: {
+    content?: string
+    width?: number
+    height?: number
+  }
 }
 
 export interface RedditResponse {
